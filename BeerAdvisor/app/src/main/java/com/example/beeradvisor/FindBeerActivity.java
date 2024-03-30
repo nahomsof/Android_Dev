976fb5d4a127b213504e8137efb9622c08993206
nahomsof <nahomsileshi21@gmail.com>
2024-03-30 14:22:45 +0300

    commit

---
+++ BeerAdvisor/app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
@@ -0,0 +1,21 @@
+package com.example.beeradvisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class FindBeerActivity extends Activity {
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+}
