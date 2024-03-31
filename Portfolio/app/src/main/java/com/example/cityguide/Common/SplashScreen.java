807a7aadc057d0eb1c2292fddba351a0a35b6dbc
nahomsof <nahomsileshi21@gmail.com>
2024-04-01 02:19:02 +0300

    commit

--- Portfolio/app/src/main/java/com/example/cityguide/Common/SplashScreen.java
+++ Portfolio/app/src/main/java/com/example/cityguide/Common/SplashScreen.java
@@ -1,6 +1,14 @@
-package com.example.cityguide;
+package com.example.cityguide.Common;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashScreen extends AppCompatActivity {
+import com.example.cityguide.R;
+import com.example.cityguide.User.UserDashboard;
 
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIMER = 5000;
+    ImageView backgroundImage;
+    TextView poweredByLine;
+    Animation sideAnim, bottomAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+
         setContentView(R.layout.splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        backgroundImage = findViewById(R.id.background_image);
+        poweredByLine = findViewById(R.id.powered_by_line);
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.buttom_anim);
+        backgroundImage.setAnimation(sideAnim);
+        poweredByLine.setAnimation(bottomAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            Intent intent = new Intent(getApplicationContext(), UserDashboard.class);
+            startActivity(intent);
+            finish();
+            }
+        },SPLASH_TIMER);
+
     }
 }
