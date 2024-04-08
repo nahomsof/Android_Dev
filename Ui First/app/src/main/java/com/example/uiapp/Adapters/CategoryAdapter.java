c6d3d9c809b72bdc05fb5ba134711a326c27b416
nahomsof <nahomsileshi21@gmail.com>
2024-04-08 11:13:19 +0300

    commit

--- Ui First/app/src/main/java/com/example/uiapp/Adapters/CategoryAdapter.java
+++ Ui First/app/src/main/java/com/example/uiapp/Adapters/CategoryAdapter.java
@@ -1,2 +1,60 @@
-package com.example.uiapp.Adapters;public class CategoryAdapter {
+package com.example.uiapp.Adapters;
+
+
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.uiapp.Domains.CategoryDomain;
+import com.example.uiapp.R;
+
+import java.util.ArrayList;
+
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+        ArrayList<CategoryDomain> items;
+        public CategoryAdapter(ArrayList<CategoryDomain> items){
+            this.items = items;
+        }
+    @NonNull
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_categoriy,parent,false);
+
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
+    holder.titleTxt.setText(items.get(position).getTitle());
+    int drawableResourceId = holder.itemView.getResources().getIdentifier(items.get(position).getPicPath(),
+            "drawalbe", holder.itemView.getContext().getPackageName());
+    Glide.with(holder.itemView.getContext());
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTxt;
+        ImageView picImg;
+        @SuppressLint("WrongViewCast")
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            picImg = itemView.findViewById(R.id.Catimg);
+
+        }
+    }
 }
